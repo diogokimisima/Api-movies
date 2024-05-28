df0fed5371464c87da0ef3232493ff90e07a0a96
dkimisima@gmail.com <dkimisima@gmail.com>
2024-05-28 15:40:03 -0300

    add myddleware

--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -3,8 +3,18 @@ const UsersController = require("../controllers/UsersController")
 
 const userRoutes = Router();
 
+// function myMiddleware(request, response, next) {
+
+//     if(!request.body.isAdmin){
+//         return response.json({ message: "user unauthorized" }); 
+//     }
+
+//     next();
+// }
+
 const usersController = new UsersController();
 
+// userRoutes.use(myMiddleware)
 userRoutes.post('/', usersController.create)
 
 module.exports = userRoutes;
